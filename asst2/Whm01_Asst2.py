4c800a67e742701066504d1d98b1cfe8e0f53486
wissam-hub <wissamh.malaeb@gmail.com>
2022-10-12 22:55:31 +0300

    added 2 interactive features

--- asst2/Whm01_Asst2.py
+++ asst2/Whm01_Asst2.py
@@ -1,4 +1,5 @@
 ### Importing libraries
+from msilib.schema import MIME
 import pandas as pd
 import streamlit as st
 import plotly.express as px
@@ -22,8 +23,7 @@ if selected== "Author Information":
     st.write("The author is Wissam Malaeb, a humble AUB graduate passionate about coding!        / id: 201801182       / Nationality: Lebanese")
 
 # Importing dataset
-df=pd.read_csv("asst2/ds_salaries.csv")
-
+df=pd.read_csv("ds_salaries.csv")
 
 #Inspect the raw data if checked (check box)
 if st.checkbox('Show raw data'):
@@ -32,23 +32,28 @@ if st.checkbox('Show raw data'):
 
 #cleaning data: unused columns
 df.drop(columns=['Unnamed: 0', 'salary', 'salary_currency'], axis=1, inplace=True)
+st.header('This bar graph represents salaries in USD of the selected major & experience level vs company location ')
 
 # Salaries for the Full Timers Mid level data scientists according to the company location
-mi_ds_data = df.loc[(df.job_title == 'Data Scientist')&(df.experience_level == "MI") & (df.employment_type == "FT")]
+options_majors = st.selectbox("Select Major location", df.job_title.unique())
+options_experience = st.selectbox("Select Experience Level", df.experience_level.unique())
+
+# mi_ds_data = df.loc[(df.job_title == 'Data Scientist')&(df.experience_level == "MI") & (df.employment_type == "FT")]
+mi_ds_data = df.loc[(df.job_title == str(options_majors))&(df.experience_level == str(options_experience)) & (df.employment_type == "FT")]
 
 average_data_mid  = pd.DataFrame(mi_ds_data.groupby('company_location').mean()["salary_in_usd"]).reset_index()
 # average_data_mid
 
 ## Plot 1: Barplot
 # plot bar graph of salary in usd vs company location
-st.header('This bar graph represents salaries in USD vs company location ')
+#Select_location = average_data_mid[average_data_mid["company_location"]==options]
+# options = st.selectbox("Select Company location", average_data_mid.company_location.unique())
+# average_data_mid_temp = average_data_mid.loc[average_data_mid.company_location == str(options)]
 fig1 = px.bar(average_data_mid, x="company_location", y='salary_in_usd')
+
 st.plotly_chart(fig1)
 
-
-
 # Salaries for the Full Timers data scientists according to the company location and experience level
-
 mi_ds_data = df.loc[(df.job_title == 'Data Scientist')& (df.employment_type == "FT")]
 average_data  = pd.DataFrame(mi_ds_data.groupby(['experience_level',"company_location"]).mean())
 average_data = average_data.reset_index()
@@ -91,14 +96,44 @@ average_data_mid.loc[average_data_mid.company_location == "PL", "company_locatio
 
 #plotting interactive world map of company locations with bubble size representing salaries 
 st.header('This Interactive world map represents salaries in USD by varying bubble sizes in different company locations')
-interactive_map = px.scatter_geo(average_data_mid, locations="company_location", hover_name ="company_location", color="company_location", size="salary_in_usd",
+America = st.checkbox('America')
+Europe = st.checkbox('Europe')
+Asia = st.checkbox('Asia')
+countries_to_show = []
+asian_countries = ["AUS", "IND", "MYS", "VNM"]
+american_countries = ["CAN", "USA", "BRA","CHL", "MEX"]
+european_countries = ["GER","FRA","AUT", "CHE", "ISR", "ESP", "GBR", "HUN", "POL"]
+if America == True:
+    for x in american_countries:
+        countries_to_show.append(x)
+else:
+    for x in countries_to_show:
+        if(x in (american_countries)):
+            countries_to_show.remove(x)
+   
+if Europe:
+    for x in european_countries:
+        countries_to_show.append(x)
+else:
+    for x in countries_to_show:
+        if(x in (european_countries)):
+            countries_to_show.remove(x)
+if Asia:
+    for x in asian_countries:
+        countries_to_show.append(x)
+else:
+    for x in countries_to_show:
+        if(x in (asian_countries)):
+            countries_to_show.remove(x)
+temp = average_data_mid.loc[average_data_mid.company_location.isin(countries_to_show)] 
+interactive_map = px.scatter_geo(temp, locations="company_location", hover_name ="company_location", color="company_location", size="salary_in_usd",
                      projection="natural earth")
 st.plotly_chart(interactive_map)
 
 ## Plot 5: Colored Barplot
 # bar plotting of salart in usd vs experience level in different companies portrayed in different colors
 st.header('This bar plot represents salaries in USD vs experience level in different companies portrayed in different colors')
-fig4 = px.bar(average_data, x="experience_level", y="salary_in_usd", color="company_location")
+
+fig4 = px.bar(average_data.sort_values(by="salary_in_usd",ascending=True), x="experience_level", y="salary_in_usd", color="company_location")
 st.plotly_chart(fig4)
 
-    
